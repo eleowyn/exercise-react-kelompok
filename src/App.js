788ea6b813be09190200a20890350f07e5012fad
elshera dahlan <elshera.dahlan@gmail.com>
2024-11-03 15:51:20 +0800

    change import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import Hello from "./components/Hello"
-import World from "./components/World"
+import gen1 from "./components/Header/Generations/gen1"
+import gen2 from "./components/Header/Generations/gen2"
+import gen3 from "./components/Header/Generations/gen3"
+import gen4 from "./components/Header/Generations/gen4"
+
+import Logo from "./components/Header/Logo"
+
+import Evolution from "./components/Home/Card-Info/Evolution"
+import logo from "./components/Home/Card-Info/logo"
+import TypeEffect from "./components/Home/Card-Info/Type-Effect"
+
+import Search from "./components/Home/Searchh"
+
+import Footer from "./components/Footer"
+
 
 const App = () => {
   return (
